45f424bd4d7ad149cb0764da6f8ee414db3a0b35
SYNC\a-sato <a-sato@sync.co.jp>
2023-01-18 15:14:30 +0900

    Test file to read file contents - Since query includes JP characters, UTF-8 encoding is used to read files CreateDBandTables Script is for initializing the DB and its tables for use

--- PythonDesktop/ReadFileContents.py
+++ PythonDesktop/ReadFileContents.py
@@ -1,3 +1,24 @@
 import os
 
-path = './DB/'
+#QUERYPATH = os.getcwd() + "\\DB"
+QUERYPATH = "./DB"
+QueryFiles = []
+CREATE_TABLE_QUERY = []
+
+for filepath in os.listdir(QUERYPATH):
+	file = filepath.split('.')
+	if file[1] == "sql":
+		#print(filepath + " is a .sql file")
+		QueryFiles.append(filepath)
+
+for sqlfile in QueryFiles:
+	currpath = QUERYPATH + "\\" + sqlfile
+	#print(currpath)
+	if os.path.isfile(currpath):
+		file = open(currpath, "r", encoding="utf-8")
+		data = str(file.read())
+		file.close()
+		CREATE_TABLE_QUERY.append(data)
+
+for query in CREATE_TABLE_QUERY:
+	print(query)
