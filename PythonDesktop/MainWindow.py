00c3986c9737641f7f409162d0147436bf0953d9
SYNC\a-sato <a-sato@sync.co.jp>
2023-01-18 18:19:47 +0900

    Updated Contents page - DB connected

--- PythonDesktop/MainWindow.py
+++ PythonDesktop/MainWindow.py
@@ -10,14 +10,19 @@ class MainWindow:
         self.mainpage()
 
     def dbselected(self):
-        selection = None
+        self.selection = None
+        self.selectedtitle = None
         for widget in self.window.winfo_children():
             #print(widget.winfo_class())
             if widget.winfo_class() == "TCombobox":
-                selection = widget
+                self.selection = widget.get()
 
-        if selection != None and selection.get() != "":
-            print("The selected DB: " + selection.get())
+        if self.selection != None and self.selection != "":
+            DBNAME = self.selection.split('.')
+            if DBNAME[0] == "FE3H":
+                #print(DBNAME[0])
+                self.selectedtitle = DBNAME[0]
+            print("The selected DB: " + self.selection)
             #print(selection)
             self.changePage()
         else:
@@ -36,22 +41,21 @@ class MainWindow:
     def mainpage(self):
         root.title("My Window Class")
         root.minsize(200,150)
-        self.db_list = dataset.getDataSet()
+        self.db_path = "./DB/"
+        self.db_list = dataset.getDataSet(self.db_path)
 
         content = ttk.Frame(root)
         self.window = content
 
-        image_frame1 = ttk.Frame(content, borderwidth=5, relief="ridge", width=150, height=300)
-        image_frame2 = ttk.Frame(content, borderwidth=5, relief="ridge", width=150, height=300)
         #center_frame = ttk.Frame(content, borderwidth=0, relief="ridge", width=350, height=300)
         label = ttk.Label(content, text="Select DB", font=("Arial", 12))
-        combo = ttk.Combobox(content, width=30, values=self.db_list, font=("Arial", 9))
+        combo = ttk.Combobox(content, width=20, values=self.db_list, font=("Arial", 9))
         button = ttk.Button(content, text="Select", command=self.dbselected)
 
         content.grid(column=0, row=0, sticky=(N,W,E,S))
         #center_frame.grid(column=1, row=0, columnspan=1, rowspan=3, pady=10, sticky=(N, W, E, S))
         label.grid(column=1, row=0, padx=10, pady=10, sticky=S)
-        combo.grid(column=1, row=1,  padx=10, pady=10, sticky=(EW))
+        combo.grid(column=1, row=1,  padx=10, pady=10)
         button.grid(column=1, row=2, padx=10, pady=10)
 
         root.columnconfigure(0, weight=1)
@@ -69,14 +73,44 @@ class MainWindow:
         root.minsize(500,300)
         content = ttk.Frame(root)
         self.window = content
-        back_btn = ttk.Button(content, text="Back", command=self.changePage)
+        db_title_label = ttk.Label(content, text=self.selectedtitle, font=("Arial", 18))
+        blankframe1 = ttk.Frame(content, height=300, width=50, relief="ridge")
+        blankframe2 = ttk.Frame(content, height=300, width=50, relief="ridge")
+        centerframe = ttk.Frame(content, height=300, width=300, relief="ridge")
+        back_btn = ttk.Button(content, text="<", command=self.changePage, width=4)
 
         content.grid(column=0, row=0, sticky=(N,W,E,S))
         back_btn.grid(column=1, row=1, sticky=(N,W))
+        blankframe1.grid(column=1, row=2, rowspan=3, sticky=(N,W,E,S), pady=(0,20))
+        blankframe2.grid(column=4, row=2, rowspan=3, sticky=(N,W,E,S), pady=(0,20))
+        centerframe.grid(column=2, row=2, columnspan=2, rowspan=3, sticky=(N,W,E,S), pady=(0,20))
+        db_title_label.grid(column=2, row=1, columnspan=2)
+        root.columnconfigure(0,weight=1)
+        root.rowconfigure(0,weight=1)
+
+        content.columnconfigure(1, weight=1)
+        content.columnconfigure(2, weight=1)
+        content.columnconfigure(3, weight=1)
+        content.columnconfigure(4, weight=1)
+        content.rowconfigure(1, weight=0)
+        content.rowconfigure(2, weight=1)
+        content.rowconfigure(3, weight=1)
+        content.rowconfigure(4, weight=1)
+
+        path = self.db_path + self.selection
+        #print(path)
+        conn = sqlite3.connect(path)
+        cur = conn.cursor()
+        sql_query = """SELECT name FROM sqlite_master WHERE type='table';"""
+        cur.execute(sql_query)
+        for row in cur:
+            print(row)
+            
+
 
 class dataset:
-    def getDataSet():
-        DBPATH = os.getcwd() + "\\DB"
+    def getDataSet(path):
+        DBPATH = path
         dblist = []
         for filepath in os.listdir(DBPATH):
             file = filepath.split('.')
